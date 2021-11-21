f6bdccc88819fc5a9eb78d37aeb5f1bc77e07ba6
Dennis Mbithi <denmbithi@gmail.com>
2021-11-21 09:30:05 +0300

    Add Episode app model

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-d7plkegqc%*$&pwu&6c$$@%y6vf)q(j3xrq)^6q=317_&dd-a9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
