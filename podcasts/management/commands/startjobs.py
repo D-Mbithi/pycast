d36e9add18c29c2e57b46a5ba3c5c47fc61b31bc
dennis mbithi <denmbithi@gmail.com>
2022-01-08 12:52:34 +0300

    added rss feedparse

---
+++ podcasts/management/commands/startjobs.py
@@ -0,0 +1,44 @@
+# podcasts/management/commands/startjobs.py
+
+from django.core.management.base import BaseCommand
+
+import feedparser
+from dateutil import parser
+from podcasts.models import Episode
+
+
+def save_new_episodes(feed):
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image['href']
+
+    for item in feed.entries:
+        if not Episode.objects.filter(guid=item.guid).exists():
+            episode = Episode(
+                title = item.title,
+                description = item.description,
+                pub_date = parser.parse(item.published),
+                link =item.link,
+                image = podcast_image,
+                podcast_name = podcast_title,
+                guid = item.guid
+            )
+
+            episode.save()
+
+
+def fetch_talkpython_episodes():
+    """Fetches new episodes from RSS for The Real Python Podcast."""
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episodes(_feed)
+
+
+def fetch_talkpython_episodes():
+    """Fetches new episodes from RSS for the Talk Python to Me Podcast."""
+    _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
+    save_new_episodes(_feed)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        fetch_talkpython_episodes()
+        fetch_talkpython_episodes()
