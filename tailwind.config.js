723c843c2ad2246adac3abf9bee8400e2db4cd48
dennis mbithi <denmbithi@gmail.com>
2022-02-04 23:21:45 +0300

    Replaced bootstrap framework with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Be Vietnam Pro", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#F25F3A",
+        secondary: "#242D52",
+      },
+    },
+  },
+  plugins: [],
+};
